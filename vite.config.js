1d46a91badebd92a282c7f9d070896ed80397bff
Nikas <nsurgautas1@gmail.com>
2025-03-26 09:41:28 +0200

    push

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/module-f-react-982807/", // Pakeisk pagal savo repo pavadinimą
+  base: "/module-f-react-982807/", // Pakeisk pagal savo repo pavadinimąs
 });
